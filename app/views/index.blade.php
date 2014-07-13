01a1d2b51f6671711895f1c1996a4badac7628bd
Steph Dube <scdube7@gmail.com>
2014-07-13 00:12:47 -0400

    turned landing page into form to include all options (does not yet do anything upon submission)

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,8 +3,39 @@
 @section('content')
 	<h1>Welcome!</h1>
 	<p>What do you need?</p>
-	<ul>
-		<li><a href="/users">I want to enerate users</a></li>
-		<li><a href="/text">I want to generate text</a></li>
-	</ul>
+	<form method="POST" action ="/text">
+		Generate text?<input type="checkbox" name="text">
+			<ul>
+				<li>Select number of paragraphs: 
+					<select>
+					<?php for ($i = 1; $i < 10; $i++):?>
+						<option value="<?php echo $i;?>"><?php echo $i;?></option>
+					<?php endfor;?>
+				</select>
+				</li>
+				<li>Select data type:
+					<select>
+						<option value="html">HTML</option>
+						<option value="json">JSON</option>
+					</select>
+				</li>
+			</ul>
+		Generate users?<input type="checkbox" name="users">
+			<ul>
+				<li>Select number of users: 
+					<select>
+					<?php for ($i = 1; $i < 10; $i++):?>
+						<option value="<?php echo $i;?>"><?php echo $i;?></option>
+					<?php endfor;?>
+				</select>
+				</li>
+				<li>Select data type:
+					<select>
+						<option value="html">HTML</option>
+						<option value="json">JSON</option>
+					</select>
+				</li>
+			</ul>
+		<input type="submit" value="Submit"></br>
+	</form>
 @stop
